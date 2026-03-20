9e63e5e8063832bf28cb7c04f2e29139a5368c84
Bhoomika <bhoomikasharma873@gmail.com>
2026-03-21 03:34:01 +0530

    signup controller added

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log("MONGODB CONNCETED:",conn.connection.host)
+    }
+    catch(error){
+         
+            console.error( "ERROR CONNECTION",error)
+            process.exit(1);
+        }
+    }
--- backend/src/server.js
+++ backend/src/server.js
@@ -31,16 +31,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
+import {connectDB} from "./lib/db.js";
 dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
@@ -50,11 +55,12 @@ app.use("/api/message", messageRoutes);
 // if (process.env.NODE_ENV === "production") {
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-// app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 
 app.get("*", (_, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 
-app.listen(PORT, () => console.log("Server running on port :" + PORT));
+app.listen(PORT, () => {console.log("Server running on port :" + PORT);
+    connectDB();  });
