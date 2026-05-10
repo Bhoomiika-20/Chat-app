f0a08c0a2d5e9361945669a40fba43254c87a2af
Bhoomika <bhoomikasharma873@gmail.com>
2026-05-10 19:49:31 +0530

    message

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,10 +2,14 @@ import express from "express";
 import {signup , login,logout,updateProfile  } from "../controllers/auth.controller.js";
 import {protectRoute} from "../middleware/auth.middleware.js"
 const router = express.Router();
-
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+// router.get("/test", arcjetProtection , (req,res)=>{
+//     res.status(200).json({ message:"Test route"})
+// })
+router.use(arcjetProtection);
 router.post ("/signup",signup  )
 
-router.post("/login", login);
+router.post("/login",login);
 
 router.post("/logout",logout);
 
